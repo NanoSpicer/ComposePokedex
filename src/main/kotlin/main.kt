31369a597a5e262df2267611474df74c2c288637
Miquel Àngel <miquelrc95@outlook.es>
2021-08-08 04:22:31 +0200

    Adapt to new Window api from Compose alpha3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,3 @@
-import androidx.compose.desktop.Window
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -6,27 +5,41 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
 import model.Pokemon
 import viewmodel.PokeViewModel
 import views.PokemonUI
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.system.exitProcess
 
 val pokeVM = PokeViewModel(PokeApiClient())
 
-fun main() = Window(
-    title = "Pokédex",
-    size = IntSize(300, 600),
-    centered = false,
-    icon = ImageIO.read((File("pokemon.png")))
-) {
-    MaterialTheme {
-        MainScreen()
+fun onExit() {
+    exitProcess(0)
+}
+
+fun main() = application {
+
+    Window(
+        title = "Pokédex",
+        icon = painterResource("pokemon.png"),
+        state = WindowState(
+            size = WindowSize(500.dp, 600.dp),
+        ),
+        onCloseRequest = ::onExit
+    ){
+        MaterialTheme {
+            MainScreen()
+        }
     }
 }
+
 
 @Composable fun MainScreen() = Box (modifier = Modifier.fillMaxHeight()){
     PokeList(pokeVM)
@@ -35,24 +48,25 @@ fun main() = Window(
 
 @Composable fun BoxScope.PokeList(vm: PokeViewModel) {
     var snackbarData by remember { mutableStateOf<Pokemon?>(null) }
-    val verticalState = rememberScrollState(0f)
+    val verticalState = rememberScrollState(0)
     val pokes by vm.pokemon.collectAsState(null)
     Column (
         modifier = Modifier.fillMaxHeight().verticalScroll(verticalState).fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Row {
+            when {
+                pokes == null -> CircularProgressIndicator(Modifier.size(64.dp))
+                pokes!!.isEmpty() -> Text("No pokes to show :(!")
+                else -> for (poke in pokes.orEmpty()) {
 
-        when {
-            pokes == null -> CircularProgressIndicator(Modifier.preferredSize(64.dp))
-            pokes!!.isEmpty() -> Text("No pokes to show :(!")
-            else -> for (poke in pokes.orEmpty()) {
-                PokemonUI(poke) {
-                    println(it)
-                    snackbarData = it
+                    PokemonUI(poke) {
+                        println(it)
+                        snackbarData = it
+                    }
                 }
             }
         }
+
 
     }
     VerticalScrollbar(
